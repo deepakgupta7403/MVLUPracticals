759ee3eba7db7b0eb55f90580f24105e94533fe2
Deepak Gupta <deepakgupta7403@gmail.com>
2023-02-15 00:40:39 +0530

    first commit

---
+++ app/src/main/java/in/locate365/mvlucollege/PracticalTwo.kt
@@ -0,0 +1,14 @@
+package `in`.locate365.mvlucollege
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PracticalTwo: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.practical_two)
+    }
+
+}
