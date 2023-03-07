37840520eef68b3313a1bd4e8872a410dd2caf5d
Deepak Gupta <deepakgupta7403@gmail.com>
2023-03-07 20:57:06 +0530

    Practical Four Two added DG commit

---
+++ app/src/main/java/in/locate365/mvlucollege/four/PracticalFourTwo.kt
@@ -0,0 +1,56 @@
+package `in`.locate365.mvlucollege.four
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import `in`.locate365.mvlucollege.R
+
+
+class PracticalFourTwo : AppCompatActivity(){
+    private lateinit var etFirstName: EditText
+    private lateinit var etLastName: EditText
+    private lateinit var btnSave: Button
+    private lateinit var sharedPref: SharedPref
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.practical_four_two)
+        initComponent()
+        onClick()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        etFirstName.setText(sharedPref.firstName)
+        etLastName.setText(sharedPref.lastName)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sharedPref.putString(SharedPref.KEY_FIRST_NAME,etFirstName.text.toString())
+        sharedPref.putString(SharedPref.KEY_LAST_NAME,etLastName.text.toString())
+    }
+
+    private fun initComponent() {
+        etFirstName = findViewById(R.id.et_first_name)
+        etLastName = findViewById(R.id.et_last_name)
+        btnSave = findViewById(R.id.btn_save)
+        sharedPref = SharedPref(this)
+    }
+
+    private fun onClick() {
+        btnSave.setOnClickListener {
+            sharedPref.putString(SharedPref.KEY_FIRST_NAME,etFirstName.text.toString())
+            sharedPref.putString(SharedPref.KEY_LAST_NAME,etLastName.text.toString())
+
+            val builder: StringBuilder = StringBuilder("First Name: ")
+            builder.append(etFirstName.text.toString()).append("\n")
+            builder.append(" Last Name: ")
+            builder.append(etLastName.text.toString())
+            Toast.makeText(this,builder.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/in/locate365/mvlucollege/four/SharedPref.kt
@@ -0,0 +1,72 @@
+package `in`.locate365.mvlucollege.four
+
+import android.content.Context
+import android.content.SharedPreferences
+
+
+open class SharedPref(context: Context) {
+    var pref: SharedPreferences
+    var editor: SharedPreferences.Editor
+    private var _context: Context
+    private var PRIVATE_MODE = 0
+
+    // Init Block
+    init {
+        _context = context
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE)
+        editor = pref.edit()
+    }
+
+    val firstName: String?
+        get() = pref.getString(KEY_FIRST_NAME, "")
+    val lastName: String?
+        get() = pref.getString(KEY_LAST_NAME, "")
+
+    fun clear() {
+        editor.putString(KEY_FIRST_NAME, null)
+        editor.putString(KEY_LAST_NAME, null)
+        editor.commit()
+    }
+
+    fun putBoolean(key: String?, value: Boolean) {
+        editor.putBoolean(key, value)
+        editor.commit()
+    }
+
+    fun getBoolean(key: String?): Boolean {
+        return pref.getBoolean(key, false)
+    }
+
+    fun putString(key: String?, value: String?) {
+        editor.putString(key, value)
+        editor.commit()
+    }
+
+    fun getString(key: String?): String? {
+        return pref.getString(key, null)
+    }
+
+    fun putLong(key: String?, value: Long) {
+        editor.putLong(key, value)
+        editor.commit()
+    }
+
+    fun getLong(key: String?): Long {
+        return pref.getLong(key, 0)
+    }
+
+    fun putInt(key: String?, value: Int) {
+        editor.putInt(key, value)
+        editor.commit()
+    }
+
+    fun getInt(key: String?): Int {
+        return pref.getInt(key, 0)
+    }
+
+    companion object {
+        private const val PREF_NAME = "mvluCollege"
+        const val KEY_FIRST_NAME = "firstName"
+        const val KEY_LAST_NAME = "lastName"
+    }
+}
